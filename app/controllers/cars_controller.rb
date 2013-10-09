cb0a158b4f47f2a8cf366eace5c7d86f58a3508f
MitulP91 <mitulp91@gmail.com>
2013-10-09 09:43:31 -0400

    screw shit up

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,15 +4,22 @@ class CarsController < ApplicationController
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    car = Car.create(params[:car])
+    redirect_to car_path(car)
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    car = Car.find(params[:id])
+    car.update_attributes(params[:car]);
+    redirect_to car_path(car);
   end
 
   def destroy
